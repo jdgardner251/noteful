0f3bed881be6962458c46ce5d0b59ddd0a5afab8
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-11-03 12:54:41 -0600

    working on post request in AddFolder

--- src/AddFolder.js
+++ src/AddFolder.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function AddFolder() {
-    return (
-        <button>Add Folder</button>
-    )
+const AddFolder = () => {
+  const [click, setClick] = useState({ clicked: false });
+  const [folderName, setFolderName] = useState('')
+
+  const updateClick = () => {
+    setClick({ clicked: true })
 }
+
+const updateFolderName = (input) => {
+    setFolderName(input)
+    console.log(folderName)
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:9090/folders`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: folderName
+        })
+}
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(res.statusText)
+            }
+            return res.json();
+        })
+        .then(data => console.log('success:', data))
+    )}
+
+
+
+  return (
+    <div>
+      <button onClick={() => updateClick()}>Add Folder</button>
+      {click.clicked && (
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="name">Folder Name</label>
+          <input onChange={(e) => updateFolderName(e.target.value)} type="text" name="name" id="name"></input>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
+};
 
 export default AddFolder;
